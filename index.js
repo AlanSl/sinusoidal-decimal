438146c8ce4fa848920f2262d36638f9bad93975
AlanSl <alan.slater@nearform.com>
2018-09-25 16:35:31 +0100

    Add input validation and handle edge cases

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 'use strict'
 
 function sinusoidalDecimal (initial, min, max) {
+  validate([initial, min, max])
+  if (min > max) {
+    const switchInput = { min, max }
+    min = switchInput.max
+    max = switchInput.min
+  }
+
   if (initial > max) return 1
   if (initial < min) return 0
+  if (min === max) return 1
 
   const pi = Math.PI
   const range = max - min
@@ -15,6 +23,14 @@ function sinusoidalDecimal (initial, min, max) {
 
   // Returns position on sine curve between 0 and 1
   return ((sinusoidalPosition + 1) / 2)
+}
+
+function validate (numbers) {
+  for (var i = numbers.length - 1; i >= 0; i--) {
+    const num = numbers[i]
+    if (typeof num !== 'number') throw new Error(`Invalid type ${typeof num} passed as sinusoidalDecimal argument [${i}]`)
+    if (Number.isNaN(num)) throw new Error(`NaN passed as sinusoidalDecimal argument [${i}]`)
+  }
 }
 
 module.exports = sinusoidalDecimal
